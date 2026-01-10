5119838011c47650926a0ee0b1bcff8dc31a0f4a
Darshan262003 <darudarshan2207@gmail.com>
2026-01-10 09:46:51 +0530

    Update response message to include Kubernetes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app=express()
 const PORT=5000
 app.get('/',(req,res)=>{
-res.send("I am from GIT to JENKINS to DOCKER")
+res.send("I am from GIT to JENKINS to DOCKER to Kubernetes")
 })
 app.listen(PORT,()=>{
 console.log(`the app is running in port${PORT}`)
